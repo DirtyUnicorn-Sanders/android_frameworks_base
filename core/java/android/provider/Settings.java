bf8337ddb07f2807c618fbaca387bf7d967ca2db
PMS22 <prathams99@rediff.com>
2018-07-25 10:50:07 +0000

    SystemUI: Add Gaming mode tile
    Enabling the tile will :-
    - Show a "First-click" informative dialog
    - Change ringer mode to vibrate
    - Increase the media volume to max
    - Disable Heads Up
    - Turn off Hardware keys
    
    - Turning off the tile will change all the values back to default
    - I haven't enabled Expanded desktop as I dont think any game runs
      in non expanded mode, so better to keep it clean.
    - Duplicate the SystemSetting class from SecureSetting one grabbing
      the System Settings string :-
      https://github.com/FireHound/android_frameworks_base/blob/o8.1/packages/SystemUI/src/com/android/systemui/qs/SecureSetting.java
    
    Change-Id: I6d9440498c0649dad0cdf38ec7c2e25ec610406e
    Signed-off-by: PMS22 <prathams99@rediff.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -4952,6 +4952,13 @@ public final class Settings {
         public static final String IMMERSIVE_RECENTS = "immersive_recents";
 
         /**
+         * Whether to enable gaming mode or not
+         *
+         * @hide
+         */
+        public static final String ENABLE_GAMING_MODE = "enable_gaming_mode";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
